67b44609710012f678bfa078c234b24c318cf525
jipang9 <kiang18@naver.com>
2023-01-16 17:40:31 +0900

    [#5-3] 버전최신화
    버전최신화

--- src/main/java/Team2/com/item/controller/ItemController.java
+++ src/main/java/Team2/com/item/controller/ItemController.java
@@ -7,9 +7,6 @@ import Team2.com.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
