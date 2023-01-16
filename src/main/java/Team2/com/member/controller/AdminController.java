3382276f1dd4a51a4e6148a8481090c01f588a27
jipang9 <kiang18@naver.com>
2023-01-16 14:12:57 +0900

    [#5-1] 전체 프로젝트 세팅
    Entityt 설계 완료 및 gitignore 적용

---
+++ src/main/java/Team2/com/member/controller/AdminController.java
@@ -0,0 +1,15 @@
+package Team2.com.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+public class AdminController {
+
+//    @GetMapping("/members")
+//    public ResponseEntity<Member>
+}
