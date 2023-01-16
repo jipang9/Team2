fb3aa0c1dfa9c7a33e3bbb4d493222053c110161
이재원 <jenyglee_30@naver.com>
2023-01-16 15:45:35 +0900

    [#2-1] order 레포지토리 추가, order 서비스 추가, Order, OrderItem, Item 엔티티에 생성자함수 추가

---
+++ src/main/java/Team2/com/order/controller/OrderController.java
@@ -0,0 +1,25 @@
+package Team2.com.order.controller;
+
+
+import Team2.com.item.entity.Item;
+import Team2.com.member.entity.Member;
+import Team2.com.order.entity.Order;
+import Team2.com.order.repository.OrderRepository;
+import Team2.com.order.service.OrderService;
+import Team2.com.orderItem.entity.OrderItems;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/customer")
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping("/orders")
+    public void createOrder(){
+        orderService.createOrder();
+    }
+}
