46a3b2b64be4437a519355aa632614dc7399f25a
이재원 <jenyglee_30@naver.com>
2023-01-17 19:18:19 +0900

    [#2-1] 주문 부분 @AuthenticationPrincipal 사용해서 Member 객체 가져올 수 있도록 변경

--- src/main/java/Team2/com/order/controller/OrderController.java
+++ src/main/java/Team2/com/order/controller/OrderController.java
@@ -1,16 +1,14 @@
 package Team2.com.order.controller;
 
-import Team2.com.member.entity.Member;
-import Team2.com.member.repository.MemberRepository;
 import Team2.com.order.dto.OrderDto;
 import Team2.com.order.service.OrderService;
-import Team2.com.security.jwt.JwtUtil;
-import io.jsonwebtoken.Claims;
+import Team2.com.security.details.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import javax.servlet.http.HttpServletRequest;
+
 import java.util.List;
 
 @RestController
@@ -18,26 +16,11 @@ import java.util.List;
 @RequestMapping("/api/customer")
 public class OrderController {
     private final OrderService orderService;
-    private final MemberRepository memberRepository;
-    private final JwtUtil jwtUtil;
 
     @PostMapping("/orders")
-    public ResponseEntity createOrder(@RequestBody OrderDto.Request requestOrderDto, HttpServletRequest request){
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;
-        if(token != null){
-            if (jwtUtil.validateToken(token)) {
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new IllegalArgumentException("Token Error");
-            }
-            Member member = memberRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
-            );
-            orderService.order(requestOrderDto.getItems(), member);
-            return new ResponseEntity("주문이 완료되었습니다.", HttpStatus.OK);
-        }
-        return new ResponseEntity("주문을 실패했습니다.", HttpStatus.BAD_REQUEST);
+    public ResponseEntity createOrder(@RequestBody OrderDto.Request requestOrderDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        orderService.order(requestOrderDto.getItems(), userDetails.getMember());
+        return new ResponseEntity("주문이 완료되었습니다.", HttpStatus.OK);
     }
 
     @GetMapping("/orders")
