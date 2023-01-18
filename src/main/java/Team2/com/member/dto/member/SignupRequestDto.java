03bea534f57a6ac4c3e55f482385f73cb8bbc013
kyung-Park <ruddnjs5816@naver.com>
2023-01-18 14:24:02 +0900

    [#2-6] 내 프로필 조회, 정규식 표현 추가

--- src/main/java/Team2/com/member/dto/member/SignupRequestDto.java
+++ src/main/java/Team2/com/member/dto/member/SignupRequestDto.java
@@ -4,11 +4,18 @@ import Team2.com.member.entity.Member;
 import Team2.com.member.entity.MemberRoleEnum;
 import lombok.Getter;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 public class SignupRequestDto {
 
+    @Size(min = 4,max = 12,message ="아이디는 4에서 12자 사이 입니다.")
+    @Pattern(regexp = "[a-z0-9]*$",message = "아이디 형식이 일치하지 않습니다.")
     private String username;
 
+    @Size(min = 8,max = 15,message ="비밀번호는 8에서 15자 사이 입니다.")
+    @Pattern(regexp = "[a-zA-Z0-9`~!@#$%^&*()_=+|{};:,.<>/?]*$",message = "비밀번호 형식이 일치하지 않습니다.")
     private String password;
 
     //private String email;
