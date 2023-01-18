d82c673bb3040ec5721ba0bd28a5497ce939ecb3
이재원 <jenyglee_30@naver.com>
2023-01-18 11:22:09 +0900

    [#2-3] OrderService 클래스를 OrderService 인터페이스와 OrderServiceImpl 클래스로 분리

--- src/main/java/Team2/com/order/service/OrderService.java
+++ src/main/java/Team2/com/order/service/OrderService.java
@@ -1,124 +1,17 @@
 package Team2.com.order.service;
 
-import Team2.com.item.entity.Item;
-import Team2.com.item.repository.ItemRepository;
 import Team2.com.member.entity.Member;
 import Team2.com.order.dto.OrderRequestItemDto;
 import Team2.com.order.dto.OrderResponseDto;
 import Team2.com.order.dto.OrderResultDto;
-import Team2.com.order.entity.Order;
-import Team2.com.order.repository.OrderRepository;
-import Team2.com.orderItem.entity.OrderItems;
-import Team2.com.orderItem.repository.OrderItemsRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
-@Service
-@RequiredArgsConstructor
-public class OrderService {
-    private final OrderRepository orderRepository;
-    private final OrderItemsRepository orderItemsRepository;
-    private final ItemRepository itemRepository;
-
-    public void order(List<OrderRequestItemDto> items, Member member) {
-        // 1. 주문할 Item 불러오기
-        ArrayList<Long> orderItemIds = new ArrayList();
-        for (OrderRequestItemDto item : items) {
-            Item findItem = itemRepository.findById(item.getId()).orElseThrow(
-                    () -> new IllegalArgumentException("상품이 존재하지 않습니다.")
-            );
-
-            // 2. OrderItem 만들기
-            OrderItems orderItems = OrderItems.createOrderItems(findItem, item.getCount());
-            OrderItems saveOrderItem = orderItemsRepository.saveAndFlush(orderItems);
-            orderItemIds.add(saveOrderItem.getId());
-        }
-
-        // 3. OrderItem 가져오기
-        List<OrderItems> orderItems = new ArrayList<>();
-
-        for (Long orderItemId : orderItemIds) {
-            Optional<OrderItems> findOrderItem = orderItemsRepository.findById(orderItemId);
-            orderItems.add(findOrderItem.get());
-        }
-
-        // 4. Order 테이블에 저장하기
-        Order order = Order.createOrder(member, orderItems);
-        orderRepository.saveAndFlush(order);
-    }
-
-    public OrderResultDto getOrders(int offset, int limit) {
-        PageRequest pageRequest = PageRequest.of(offset, limit, Sort.by(Sort.Direction.ASC, "id"));
-        Page<Order> page = orderRepository.findAll(pageRequest);
-        Page<OrderResponseDto> map = page.map(order -> new OrderResponseDto(order.getId(), order.getMember().getUsername(), order.getOrderItems()));
-        List<OrderResponseDto> content = map.getContent(); // Order 배열
-        long totalCount = map.getTotalElements(); // Order 전체 개수
-
-        OrderResultDto result = new OrderResultDto<>(offset, totalCount, content);
-
-        return result;
-    }
-
-    //(판매자) 주문내역 전체 조회
-    @Transactional
-    public List<OrderResponseDto> getAllCustomerBuyList(PageRequest pageRequest, String sellerName) {
-
-        Page<Order> page = orderRepository.findAll(pageRequest);
-        List<OrderResponseDto> resultList = new ArrayList<>();
-
-        Iterator<Order> keys = page.iterator();
-        while( keys.hasNext() ){
-            Order key = keys.next();
-
-            for(int i=0; i<key.getOrderItems().size(); i++){
-                if(sellerName.equals(key.getOrderItems().get(i).getItem().getMember().getUsername())){
-                    resultList.add(new OrderResponseDto(key.getId(), key.getMember().getUsername(), key.getOrderItems()));
-                }
-            }
-        }
-        return resultList;
-    }
-
-    //(판매자)주문 내역 조회
-    @Transactional(readOnly = true)
-    public OrderResponseDto getCustomerBuyItem(Long orderId, String sellerName) {
-
-        //주문조회
-        Order order = orderRepository.findById(orderId).orElseThrow(()-> new IllegalArgumentException("주문내역이 존재하지 않습니다."));
-
-        //판매자 상품이 맞는지 확인
-        for(int i=0; i<order.getOrderItems().size(); i++){
-            if(!order.getOrderItems().get(0).getItem().getMember().getUsername().equals(sellerName)){
-                throw new IllegalArgumentException("판매자의 상품과 일치하지 않습니다.");
-            }
-        }
-        return new OrderResponseDto(order.getId(), order.getMember().getUsername(), order.getOrderItems());
-    }
-
-    //(판매자)주문완료처리
-    @Transactional
-    public void orderCompleteProceeding(Long orderId, String sellerName) {
-
-        //주문조회
-        Order order = orderRepository.findById(orderId).orElseThrow(()-> new IllegalArgumentException("주문내역이 존재하지 않습니다."));
-
-        //판매자 상품이 맞는지 확인
-        for(int i=0; i<order.getOrderItems().size(); i++){
-            if(!order.getOrderItems().get(0).getItem().getMember().getUsername().equals(sellerName)){
-                throw new IllegalArgumentException("판매자의 상품과 일치하지 않습니다.");
-            }
-        }
-        //주문상태 변경
-        order.updateOrderStatus();
-    }
-
+public interface OrderService {
+    void order(List<OrderRequestItemDto> items, Member member); // 주문하기
+    OrderResultDto getOrders(int offset, int limit); // (고객) 주문내역 전체 조회
+    List<OrderResponseDto> getAllCustomerBuyList(PageRequest pageRequest, String sellerName); //(판매자) 주문내역 전체 조회
+    OrderResponseDto getCustomerBuyItem(Long orderId, String sellerName); //(판매자)주문 내역 조회
+    void orderCompleteProceeding(Long orderId, String sellerName); //(판매자)주문완료처리
 }
