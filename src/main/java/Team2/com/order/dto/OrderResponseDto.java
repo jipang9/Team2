bd83aa411b8443c7b9938a1ef90340bc90ec6753
이재원 <jenyglee_30@naver.com>
2023-01-19 14:21:57 +0900

    [#2-1] 주문 조회 시 상품 가격(price), 상품 주문 수량(count), 주문 총합 가격(totalPrice) 추가

--- src/main/java/Team2/com/order/dto/OrderResponseDto.java
+++ src/main/java/Team2/com/order/dto/OrderResponseDto.java
@@ -10,14 +10,24 @@ import java.util.List;
 @Getter
 public class OrderResponseDto {
     private Long id;
+    private int totalPrice;
     private List<OrderItemsResponseDto> orderItemsList = new ArrayList<>();
 
 
     public OrderResponseDto(Long id, List<OrderItems> orderItems) {
         this.id = id;
+        this.totalPrice = totalPrice(orderItems);
         for (OrderItems o : orderItems) {
-            OrderItemsResponseDto orderItemsDto = new OrderItemsResponseDto(o.getItem().getId(), o.getItem().getName());
+            OrderItemsResponseDto orderItemsDto = new OrderItemsResponseDto(o.getItem().getId(), o.getItem().getName(), o.getItem().getPrice(), o.getCount());
             orderItemsList.add(orderItemsDto);
         }
+    }
+
+    public int totalPrice(List<OrderItems> orderItems){
+        int sum = 0;
+        for (OrderItems o : orderItems) {
+            sum += o.getItem().getPrice() * o.getCount();
+        }
+        return sum;
     }
 }
